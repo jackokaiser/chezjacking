f51a186908d99d036ff0da120feb33b637a22012
Ubuntu <ubuntu@ip-172-31-17-148.us-west-2.compute.internal>
2013-07-09 19:21:47 +0000

    buffer index

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
 var express = require("express");
+var fs = require("fs");
+
+
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+    var buffer=fs.readFileSync("index.html");
+
+    response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
