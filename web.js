1879a4b8419c4975953a972b172e98229e6d2788
jackokaiser <jacko.kaiser@gmail.com>
2013-07-31 21:54:45 +0200

    url routing

--- web.js
+++ web.js
@@ -1,16 +1,37 @@
 var express = require("express");
 var fs = require("fs");
-
+var url = require("url");
 
 var app = express();
 app.use(express.logger());
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
+var publicFiles = [
+    "about",
+    "home",
+    "contact",
+    "projects",
+    "raytracer"
+];
 
-    var buffer=fs.readFileSync("public/html/projects.html");
+var simpleRender = function(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    var buffer=fs.readFileSync("public/html"+pathname+".html");
+    // var buffer=fs.readFileSync("public/html/index.html");
+    response.send(buffer.toString());
+    // response.render("public/html/" + pathname + ".html");
+};
 
+
+app.get('/', function(request,response) {
+    var buffer=fs.readFileSync("public/html/home.html");
     response.send(buffer.toString());
 });
+
+for (var i=0;i<publicFiles.length;i++)
+{
+    app.get('/'+publicFiles[i],simpleRender);
+}
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
