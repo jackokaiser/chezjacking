ed27848804ba92482578ae9b94858761e2ca7009
jackokaiser <jacko.kaiser@gmail.com>
2013-07-31 20:21:16 +0200

    Revert "projects updated, try to include url parsing"
    This reverts commit e498de803b6c61297949934cbdc91579190d8885.
    
    Conflicts:
    
    	public/css/base.css
    	public/html/index.html

--- web.js
+++ web.js
@@ -1,16 +1,14 @@
 var express = require("express");
 var fs = require("fs");
-var url = require("url");
+
 
 var app = express();
 app.use(express.logger());
-app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-    // var pathname = url.parse(request.url).pathname;
-    // response.render("html/" + pathname + ".html");
-    // var buffer=fs.readFileSync("public/" + pathname);
-    var buffer=fs.readFileSync("public/html/index.html");
+
+    var buffer=fs.readFileSync("templates/index.html");
+
     response.send(buffer.toString());
 });
 
